88e34714d1286b19c18f8e9217c5431b256f54cc
GT-SteveG <97601539+GT-SteveG@users.noreply.github.com>
2024-06-29 15:06:49 +0100

    software.ps1

--- software.ps1
+++ software.ps1
@@ -7,18 +7,13 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocola
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
 
 # Install programs with Chocolatey
-choco install pestudio -y
-choco install volatility -y
 choco install filezilla -y
 choco install 7zip -y
 choco install hxd -y
-choco install sysinternals -y
 choco install openoffice -y
 choco install autopsy -y
 choco install adobereader -y
 choco install googlechrome -y
 choco install notepadplusplus -y
 choco install sublimetext3 -y
-choco install arsenalimagemounter -y
 choco install microsoftazurestorageexplorer -y
-choco install chainsaw -y
