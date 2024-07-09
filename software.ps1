75075130c5b4c60eaa1739da8c40aa05ba2397ea
AlasdairClarkGT <91890411+AlasdairClarkGT@users.noreply.github.com>
2024-07-09 16:19:30 +0100

    Update software.ps1

--- software.ps1
+++ software.ps1
@@ -6,12 +6,8 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocola
 # Refresh environment variables
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
 
+# Disable requirement for confirmation
+choco feature enable -n=allowGlobalConfirmation
+
 # Install programs with Chocolatey
-choco install filezilla -y
-choco install 7zip -y
-choco install adobereader -y
-choco install googlechrome -y
-choco install notepadplusplus -y
-choco install sublimetext3 -y
-choco install microsoftazurestorageexplorer -y
-choco install openoffice -y
+choco install filezilla 7zip git googlechrome hxd vlc python adobereader notepadplusplus sublimetext3 microsoftazurestorageexplorer sysinternals openoffice
